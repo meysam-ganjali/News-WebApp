c73cdb74bde11d12f06e4bbc06c7d9f62b270d64
meysam-ganjali <meysam.ghanjali1365@hotmail.com>
2022-12-01 07:15:41 -0800

    handle Get Parent Category

---
+++ News.Web/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using News.Application.Repository.IRepository;
+
+namespace News.Web.Areas.Admin.Controllers
+{
+
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CategoryController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var menuItemList = await _unitOfWork.ParentCategory.GetAll(includeProperties: "NewsCategories");
+            return View(menuItemList);
+        }
+    }
+}
--- News.Web/Program.cs
+++ News.Web/Program.cs
@@ -49,6 +49,10 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapControllerRoute(
+    name: "areas",
+    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
 app.MapRazorPages();
 app.Run();
 
