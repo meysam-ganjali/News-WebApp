0bca50e3e08fbf8d617e04f5c95554f6bfd913d5
meysam-ganjali <meysam.ghanjali1365@hotmail.com>
2022-12-01 04:44:38 -0800

    Add EF Core Dependencies, Add Identity

--- News.Web/Program.cs
+++ News.Web/Program.cs
@@ -1,7 +1,30 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using News.Web.Data;
+
+#region Variables
+
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("DatabaseContextConnection") ?? throw new InvalidOperationException("Connection string 'DatabaseContextConnection' not found.");
+
+#endregion
+
+#region Register Services
+
+builder.Services.AddDbContext<DatabaseContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<IdentityUser>()
+    .AddEntityFrameworkStores<DatabaseContext>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+
+#endregion
+
+#region Piplines
+
 
 var app = builder.Build();
 
@@ -17,11 +40,15 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication();;
 
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-
+app.MapRazorPages();
 app.Run();
+
+
+#endregion
